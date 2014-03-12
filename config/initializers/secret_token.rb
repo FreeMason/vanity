e7763298f53ff6520da41971cd146922d25e8762
Garren Givens <garren.givens@gmail.com>
2014-03-11 23:07:45 -0400

    added a couple static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysite::Application.config.secret_token = '6be689d57f4f9e0d76a9f25367691443bd251c0979c253284183b19e98c7dc4d01dd8d893bb9f27aaa19e8eb2e79372c3e7aea37dea7dafda9a5e838c8f38e34'
